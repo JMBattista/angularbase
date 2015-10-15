c4233115cff36bbbd4622827aeee4d0179d7ceca
John Battista <jmbattista@gmail.com>
2015-10-15 00:13:20 -0700

    turn js-dev into a function

--- gulp/javascript.js
+++ gulp/javascript.js
@@ -27,14 +27,14 @@ gulp.task('lint', function(){
 	return gulp.src(config.source)
         .pipe(plumber())
         .pipe(js)
-	    .pipe(jshint())
-	    .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
-        .pipe(jshint.reporter('fail'))
-        .pipe(jscs())
+            .pipe(jshint())
+            .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+            .pipe(jshint.reporter('fail'))
+            .pipe(jscs())
         .pipe(js.restore)
         .pipe(ts)
-        .pipe(tslint())
-        .pipe(tslint.report('verbose'))
+            .pipe(tslint())
+            .pipe(tslint.report('verbose'))
         .pipe(ts.restore)
 });
 
@@ -56,7 +56,20 @@ gulp.task('js', function () {
         .pipe(livereload());
 });
 
-gulp.task('js-dev', function () {
+gulp.task('js:watch', function () {
+    livereload.listen();
+    processJavascriptDev();
+    watch(config.source, function(vinyl) {
+        if (vinyl.event === 'unlink') {                   // if a file is deleted, forget about it
+            delete cached.caches.scripts[vinyl.path];       // gulp-cached remove api
+            remember.forget('scripts', vinyl.path);         // gulp-remember remove api
+        }
+
+        processJavascriptDev();
+    });
+});
+
+function processJavascriptDev() {
     var ts = config.filter.ts();
 
     return gulp.src(config.source)
@@ -73,17 +86,4 @@ gulp.task('js-dev', function () {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.dest + 'js/'))
         .pipe(livereload());
-});
-
-gulp.task('js:watch', ['js-dev'], function () {
-    livereload.listen();
-    watch(config.source, function(vinyl) {
-        console.log(vinyl.path);
-        if (vinyl.event === 'unlink') {                   // if a file is deleted, forget about it
-            delete cached.caches.scripts[vinyl.path];       // gulp-cached remove api
-            remember.forget('scripts', vinyl.path);         // gulp-remember remove api
-        }
-
-        gulp.start('js-dev');
-    });
-});
+};
