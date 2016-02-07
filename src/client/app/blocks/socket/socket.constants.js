fab5170801f2a9d1eafdb45215370761e6ec04d1
Colin Davidson <ctdavids@gmail.com>
2016-02-06 17:21:04 -0800

    Move socket constants out of module.js

---
+++ src/client/app/blocks/socket/socket.constants.js
@@ -0,0 +1,39 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('blocks.socket')
+        .constant('SOCKET_DEFAULTS', (function () {
+            return {
+                TIMEOUT_MS: 20000,
+                REQUIRE_ACKNOWLEDGEMENT: true
+            }
+        })())
+        .constant('SOCKET_SEND', (function () {
+            return {
+                FAILED: 'failed',
+                SENDING: 'sending',
+                SUCCESS: 'success'
+            }
+        })())
+        .constant('SOCKET_STATUS', (function () {
+            let CONNECT = 'connect',
+                ERROR = 'error',
+                DISCONNECT = 'disconnect',
+                RECONNECT = 'reconnect',
+                RECONNECT_ATTEMPT = 'reconnect_attempt',
+                RECONNECT_ERROR = 'reconnect_error',
+                RECONNECT_FAILED = 'reconnect_failed';
+
+            return {
+                CONNECT: CONNECT,
+                ERROR: ERROR,
+                DISCONNECT: DISCONNECT,
+                RECONNECT: RECONNECT,
+                RECONNECT_ATTEMPT: RECONNECT_ATTEMPT,
+                RECONNECT_ERROR: RECONNECT_ERROR,
+                RECONNECT_FAILED: RECONNECT_FAILED,
+                ALL: [CONNECT, ERROR, DISCONNECT, RECONNECT, RECONNECT_ATTEMPT, RECONNECT_ERROR, RECONNECT_FAILED]
+            }
+        })())
+})();
--- src/client/app/blocks/socket/socket.module.js
+++ src/client/app/blocks/socket/socket.module.js
@@ -1,27 +1,9 @@
 (function () {
     'use strict';
 
-    angular.module('blocks.socket', [
-        'blocks.logger'
-    ]).constant('SOCKET_EVENT', (function () {
-        let CONNECT = 'connect',
-            ERROR = 'error',
-            DISCONNECT = 'disconnect',
-            RECONNECT = 'reconnect',
-            RECONNECT_ATTEMPT = 'reconnect_attempt',
-            RECONNECT_ERROR = 'reconnect_error',
-            RECONNECT_FAILED = 'reconnect_failed';
-
-        return {
-            CONNECT: CONNECT,
-            ERROR: ERROR,
-            DISCONNECT: DISCONNECT,
-            RECONNECT: RECONNECT,
-            RECONNECT_ATTEMPT: RECONNECT_ATTEMPT,
-            RECONNECT_ERROR: RECONNECT_ERROR,
-            RECONNECT_FAILED: RECONNECT_FAILED,
-            ALL: [CONNECT, ERROR, DISCONNECT, RECONNECT, RECONNECT_ATTEMPT, RECONNECT_ERROR, RECONNECT_FAILED]
-        }
-    })());
+    angular
+        .module('blocks.socket', [
+            'blocks.logger'
+        ]);
 
 })();
